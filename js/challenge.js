0606a22189fbe74b822f4e46a568a996b80534f3
Sheila O'Donnell <sheila@contexttravel.com>
2020-03-31 14:12:19 +0000

    Automatically backed up by Learn

---
+++ js/challenge.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+let counter = document.getElementById('counter');
+let plus = document.getElementById('plus');
+let subtract = document.getElementById('minus');
+let pause = document.getElementById('pause')
+let heart = document.getElementById('heart')
+let paused = false
+let seconds = parseInt(counter.innerText)
+let clockDefault = setInterval(() => clockIncrement(), 1000);
+let favoritesList = document.querySelector("ul");
+let commentForm = document.getElementById('comment-form')
+
+
+function clockIncrement() {
+  if(!paused) {
+  seconds += 1;
+  counter.innerText = seconds
+  }
+}
+
+function addSecond() {
+  counter.innerHTML++;
+}
+
+function subtractSecond() {
+  counter.innerHTML--;
+}
+
+function pauseClock() {
+  if (paused === false) {
+    paused = true;
+    pause.innerText = 'resume';
+  }
+}
+
+function addFavorite() {
+  let favoritesCount = 1;
+  let favorite = document.createElement("li");
+  favorite.innerText = seconds + " has been liked " + favoritesCount + " time";
+  favoritesList.appendChild(favorite);
+}
+
+
+plus.addEventListener("click", addSecond);
+subtract.addEventListener("click", subtractSecond);
+pause.addEventListener('click', pauseClock);
+heart.addEventListener('click', addFavorite);
+
+})
